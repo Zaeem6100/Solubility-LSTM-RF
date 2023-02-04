b652fe9fcbe8240bb011e900a338a3250c1f4d03
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2023-02-04 22:23:00 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+import logging
+
+app = Flask(__name__)
+
+logger = logging.getLogger('Verifier')
+logger.setLevel("DEBUG")
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/singleSequence', methods=['POST'])
+def singleSequence():
+    return 'Hello World!'
+
+
+@app.route('/multipleSequence', methods=['POST'])
+def multipleSequence():
+    if request.method != 'POST':
+        return "Not Allowed", 405
+    return 'Hello World!'
+
+
+@app.route('/singleSequence/<string:sequence>', methods=['GET'])
+def singleSequence(sequence):
+    return sequence
+
+
+
+
+@app.errorhandler(404)
+def not_found():
+    return "Not Found", 404
+
+
+if __name__ == '__main__':
+    app.run()
