67f4184f39588d90ac9ef485f29bb00a1e78a7e4
Zaeem <32678310+Zaeem6100@users.noreply.github.com>
2023-02-26 00:36:59 +0500

    code

--- app.py
+++ app.py
@@ -4,27 +4,26 @@ from helper import Predictor
 
 app = Flask(__name__)
 
-app.FLASK_DEBUG = 1
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/singleSequence', methods=['GET'])
-def singleSequence():
-    return 'Hello World!'
-
-
 @app.route('/results', methods=['post'])
 def results():
-    sequences = request.form['sequences']
+    sequences = request.form['sequence']
     pred = Predictor()
     seq = "MGSVRKASAWLGLVDDNDDERYYDDDYAEGQESGEAWVTDPRVKVASETAEEKGRRIGTVTPDSFRDARGIGELFRDGVPVIINLTAMEPTDAKRVVDFAAGLTFGLRGTIERVATRVFLLTPANTEIVSGEAAGRPTDGFFNQS"
     clf = pred.RFInit()
     data = clf.predict(pred.LSTMPredict(sequences.upper()))
-    return render_template('results.html', sequences=data[0])
+    print(data[0])
+    send = ""
+    if data[0] == 1:
+        send = "Protein is Soluble"
+    else:
+        send = "Protein is Insoluble"
+    return render_template('index.html', sequences=send, input=sequences.upper())
 
 
 # @app.route('/singleSequence/<string:sequence>', methods=['GET'])
@@ -33,9 +32,9 @@ def results():
 
 
 @app.errorhandler(404)
-def not_found():
-    return "Not Found"
+def not_found(error):
+    return "Not Found", 404
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
